8f4bd6840b5686604b2d4173beb809640c18be48
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2019-11-03 14:35:11 -0300

    Updates

---
+++ precache-manifest.5275b4dc0b74e4ebd5943f79fe3fa380.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd233dd9b9dca10f85eb75f940880ece",
+    "url": "/react-coin-flip/index.html"
+  },
+  {
+    "revision": "c390379c6b97f1d1f47e",
+    "url": "/react-coin-flip/static/css/main.eb119d7f.chunk.css"
+  },
+  {
+    "revision": "29e56c31c67d71bc75e9",
+    "url": "/react-coin-flip/static/js/2.592267b4.chunk.js"
+  },
+  {
+    "revision": "c390379c6b97f1d1f47e",
+    "url": "/react-coin-flip/static/js/main.2eb41c48.chunk.js"
+  },
+  {
+    "revision": "b5717f5fd690f9dee558",
+    "url": "/react-coin-flip/static/js/runtime-main.a9a4575d.js"
+  },
+  {
+    "revision": "99a107299db97ffe8a1491d5427ba596",
+    "url": "/react-coin-flip/static/media/heads.99a10729.jpg"
+  },
+  {
+    "revision": "79f3cbf92a9127cc1df025dbfd90c169",
+    "url": "/react-coin-flip/static/media/tails.79f3cbf9.jpg"
+  }
+]);
